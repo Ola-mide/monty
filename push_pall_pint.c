9236e90a29898b651e75c3ecd4f7a1e862886f24
Abdulrahman Oyetade <abdulrahmanoyetade@gmail.com>
2022-10-08 09:30:50 -0700

    correcting the empty stack for pint

--- push_pall_pint.c
+++ push_pall_pint.c
@@ -61,7 +61,7 @@ void pint(stack_t **stack, unsigned int line_number)
 
 	if (tmp == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit_free(*stack);
 		exit(EXIT_FAILURE);
 	}
